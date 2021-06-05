afef1f409de26dc1d22d669bc9fdfb3118f74b98
Steven Ortiz <stevenortiz@knights.ucf.edu>
2021-06-04 23:37:14 -0400

    Assignment 3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,51 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 3
+ *  Copyright 2021 Steven Ortiz
+ */
+
+
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+//What is the quote? These aren't the droids you're looking for.
+//Who said it? Obi-Wan Kenobi
+//Obi-Wan Kenobi says, "These aren't the droids you're looking for."
+
+
+import java.util.Scanner;
+
+public class App {
+    static Scanner in = new Scanner(System.in);
+
+
+
+    public static void main(String[] args) {
+
+        String name = readName();
+        String Speaker = Person();
+
+        String outputString = generateOutputString(name, Speaker);
+        printOutput(outputString);
+
+    }
+    private static void printOutput(String outputString){
+        System.out.println(outputString);
+    }
+
+    private static String readName(){
+        System.out.print("What is the quote?");
+        String name = in.nextLine();
+        return name;
+    }
+    private static String Person() {
+        System.out.print("Who said it?");
+        String Speaker = in.nextLine();
+        return Speaker;
+
+    }
+
+    private static String generateOutputString(String name, String Speaker) {
+
+
+        return Speaker + " says, " + '"' + name + '"';
     }
 }
